2fd1f99e6535b3001bd650f16f2ee99c7b169852
camavep <mario.carlos.pino@gmail.com>
2021-12-14 21:16:57 -0500

    modified file fizz and fizzbuzz - module%

--- js/fizz.js
+++ js/fizz.js
@@ -1,17 +1,13 @@
 var numeros = 100;
-var divisible = false;
 
 for (let i = 1; i <= 100; i++) {
-    divisible = false;
     if (i % 3 == 0) {
         document.write("Fizz")
-        divisible = true;
     }
     if (i % 5 == 0) {
         document.write("Buzz");
-        divisible = true;
     }
-    if (!divisible) {
+    if (i%3 != 0 && i%5 !=0) {
         document.write(i);
     }
      document.write("<br/>") 
