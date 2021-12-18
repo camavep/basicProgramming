a0047dff3f36725c70b42fafca39e2d212391450
camavep <mario.carlos.pino@gmail.com>
2021-12-18 15:58:49 -0500

    validate file fizz and fizzbuzz - module%

--- js/fizz.js
+++ js/fizz.js
@@ -4,7 +4,7 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0) {
         document.write("Fizz")
     }
-    if (i % 5 == 0) {
+    if (i % 8 == 0) {
         document.write("Buzz");
     }
     if (i%3 != 0 && i%5 !=0) {
